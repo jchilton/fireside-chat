e2e03151f6abcfdb1d34de43260727e22f1f8e2c
Zach Turk <zachturk@gmail.com>
2013-11-02 16:55:25 -0700

    login intial commit

--- FireSideChat/src/com/example/firesidechat/HttpAction.java
+++ FireSideChat/src/com/example/firesidechat/HttpAction.java
@@ -1,19 +1,54 @@
 package com.example.firesidechat;
 
+import java.io.InputStream;
 import java.util.HashMap;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 public class HttpAction extends AsyncTask<String, Void, String> {
 	
 	public HttpAction() {
 		
 	}
+	
+	public void POST(String url, HashMap<String, String> params) {
+		HttpClient client = new DefaultHttpClient();
+        HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+        HttpResponse response;
+        InputStream in = null;
 
-	private void writeJSON(HashMap<String, String> params) {
+        try {
+            HttpPost post = new HttpPost(url);
+            StringEntity se = new StringEntity(writeJSON(params));  
+            se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+            post.setEntity(se);
+            response = client.execute(post);
+
+            /*Checking response */
+            if(response!=null){
+                in = response.getEntity().getContent(); //Get the data in the entity
+                Log.d("response", in.toString());
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+	}
+
+	private String writeJSON(HashMap<String, String> params) {
 		  JSONObject object = new JSONObject();
 		  try {
 			  for (String key : params.keySet()) 
@@ -25,7 +60,7 @@ public class HttpAction extends AsyncTask<String, Void, String> {
 		  } catch (JSONException e) {
 		    e.printStackTrace();
 		  }
-		  System.out.println(object);
+		  return object.toString();
 		}
 
 	@Override
--- FireSideChat/src/com/example/firesidechat/Server.java
+++ FireSideChat/src/com/example/firesidechat/Server.java
@@ -5,7 +5,12 @@ import java.util.HashMap;
 public class Server {
 	private static String url = "http://10.20.11.49/join";
 
-	public static void login(HashMap<String, String> parameters) {
-		
+	public static void login(String username, String password, String topic) {
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("username", username);
+		params.put("password", password);
+		params.put("topic", topic);
+		params.put("oneonone", "n");
+		new HttpAction().POST(url, params);
 	}
 }
