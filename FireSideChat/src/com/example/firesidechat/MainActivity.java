9cba7977d5fa0acc0fa90d31075f233f32ae5b42
BioSt0rm <tim.c.larson@gmail.com>
2013-11-02 15:49:34 -0700

    Added chat Activity and linked main button

--- FireSideChat/src/com/example/firesidechat/MainActivity.java
+++ FireSideChat/src/com/example/firesidechat/MainActivity.java
@@ -11,13 +11,17 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -29,6 +33,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+        ImageButton init_chat = (ImageButton) findViewById(R.id.chat_init_button);
+        init_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, chat_activity.class);
+                startActivity(intent);
+            }
+        });
 	}
 
 	@Override
