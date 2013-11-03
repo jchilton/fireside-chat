e6c5e566773ed5de467fb15e65524a805732b8b7
BioSt0rm <tim.c.larson@gmail.com>
2013-11-03 06:01:02 -0800

    CALM DOWN

--- FireSideChat/src/com/example/firesidechat/ChatActivity.java
+++ FireSideChat/src/com/example/firesidechat/ChatActivity.java
@@ -41,7 +41,17 @@ public class ChatActivity extends Activity {
 
             }
         });
+        
+        
     }
+    public void displayMessage(String msg_data, String username, String timestamp) { 
+    	//will post message
+    	//timesteamp
+    	//then call pull message with boolean overRideTimer=True
+    }
+
+    
+    
 
 
 }
