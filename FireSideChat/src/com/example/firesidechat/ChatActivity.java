0761968e12ccd3f186c607a2567c0e795f82d68a
BioSt0rm <tim.c.larson@gmail.com>
2013-11-03 10:30:04 -0800

    borked

--- FireSideChat/src/com/example/firesidechat/ChatActivity.java
+++ FireSideChat/src/com/example/firesidechat/ChatActivity.java
@@ -10,6 +10,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -32,98 +33,100 @@ import com.example.firesidechat.web.SubmitMessageRequestTask;
 /**
  * Created by timlarson on 11/2/13.
  */
-public class ChatActivity extends Activity implements HasHttpPostCallback{
+public class ChatActivity extends Activity implements HasHttpPostCallback {
 	String username;
 	String password;
 	String topicName;
 	Integer topicId;
-	
+
 	MessageList messages;
-	
+
 	Timer messagesTimer = new Timer();
-	final Long TIMER_DELAY = (long) 20 * 1000;
+	final Long TIMER_DELAY = (long) 1000;
 	String lastTimeStamp = null;
-	
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.chat_activity);
-        
-        messages = new MessageList();
-        
-    	String time = new Timestamp(System.currentTimeMillis()).toString();
-        lastTimeStamp = time.substring(0, time.lastIndexOf('.'));
-        
-		//sets keyboard to start hidden until soft input selected
 
-        
-        //get intent from previous activity
-        Intent intent = getIntent();
-        username = intent.getStringExtra("username");
-        password = intent.getStringExtra("password");
-        topicName = intent.getStringExtra("topicName");
-        topicId = intent.getIntExtra("topicId", 0);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.chat_activity);
 
-        TextView tag = (TextView) findViewById(R.id.chat_tags);
-        tag.setText("Topic: " + topicName);
-        
-        messagesTimer.scheduleAtFixedRate(new TimerTask() {
+		messages = new MessageList();
+
+		String time = new Timestamp(System.currentTimeMillis()).toString();
+		lastTimeStamp = time.substring(0, time.lastIndexOf('.'));
+
+		// sets keyboard to start hidden until soft input selected
+
+		// get intent from previous activity
+		Intent intent = getIntent();
+		username = intent.getStringExtra("username");
+		password = intent.getStringExtra("password");
+		topicName = intent.getStringExtra("topicName");
+		topicId = intent.getIntExtra("topicId", 0);
+
+		TextView tag = (TextView) findViewById(R.id.chat_tags);
+		tag.setText("Topic: " + topicName);
+
+		messagesTimer.scheduleAtFixedRate(new TimerTask() {
 			@Override
 			public void run() {
 				updateMessages();
 			}
 		}, 0, TIMER_DELAY);
 
-        final TextView msg_data = (TextView) findViewById(R.id.message_text_field);
-        Button msg_btn = (Button) findViewById(R.id.message_send_button);
-        msg_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //http://stackoverflow.com/questions/6369062/how-do-i-add-elements-dynamically-to-a-view-created-with-xml
-            	//on click send HTTPPostTask
-            	submitMessage(msg_data.getText().toString());
-            	
-            	displayUpdate();
-            	
-            	msg_data.setText("");
-            	
+		final TextView msg_data = (TextView) findViewById(R.id.message_text_field);
+		Button msg_btn = (Button) findViewById(R.id.message_send_button);
+		msg_btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				// http://stackoverflow.com/questions/6369062/how-do-i-add-elements-dynamically-to-a-view-created-with-xml
+				// on click send HTTPPostTask
+				submitMessage(msg_data.getText().toString());
 
-            }
-        });
-        
-        
-    }
-    public void displayMessage(String msg_data, String username, String timestamp) { 
-    	//will post message
-    	//timesteamp
-    	//then call pull message with boolean overRideTimer=True
-    	
-    }
+				displayUpdate();
 
-    
-    
+				msg_data.setText("");
+
+			}
+		});
+
+	}
+
+	private void showMessage(String message) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(message);
+		builder.show();
+	}
+
+	public void displayMessage(String msg_data, String username,
+			String timestamp) {
+		// will post message
+		// timesteamp
+		// then call pull message with boolean overRideTimer=True
+
+	}
 
 	public void updateMessages() {
-        HashMap<String, String> requestVals = new HashMap<String, String>();
-        requestVals.put("username", username);
-        requestVals.put("password", password);
-        
-        Log.d("TimeStamp", "asking for msgs later than " + lastTimeStamp);
-        requestVals.put("timestamp", lastTimeStamp);
-    	
-    	JSONObject obj = new JSONObject(requestVals);
-        try {
-        	obj.put("topic_id", Integer.valueOf(topicId));
-        	new SearchMessagesRequestTask(this).execute(Server.MESSAGES_URL, obj.toString());
-        }
-        catch (JSONException e) { }
-        
-    	String time = new Timestamp(System.currentTimeMillis()).toString();
-        lastTimeStamp = time.substring(0, time.lastIndexOf('.'));
-        
-        
-        for(Message m : messages.getMessages()) {
-        	Log.d("Messages", m.toString()+" ");
-        }
+		HashMap<String, String> requestVals = new HashMap<String, String>();
+		requestVals.put("username", username);
+		requestVals.put("password", password);
+
+		Log.d("TimeStamp", "asking for msgs later than " + lastTimeStamp);
+		requestVals.put("timestamp", lastTimeStamp);
+
+		JSONObject obj = new JSONObject(requestVals);
+		try {
+			obj.put("topic_id", Integer.valueOf(topicId));
+			new SearchMessagesRequestTask(this).execute(Server.MESSAGES_URL,
+					obj.toString());
+		} catch (JSONException e) {
+		}
+
+		String time = new Timestamp(System.currentTimeMillis()).toString();
+		lastTimeStamp = time.substring(0, time.lastIndexOf('.'));
+
+		for (Message m : messages.getMessages()) {
+			Log.d("Messages", m.toString() + " ");
+		}
 	}
 
 	public void addMessages(String response) {
@@ -133,70 +136,82 @@ public class ChatActivity extends Activity implements HasHttpPostCallback{
 			JSONArray jsonArray = new JSONObject(response).getJSONArray("data");
 			for (int i = 0; i < jsonArray.length(); i++) {
 				JSONObject jsonObj = jsonArray.getJSONObject(i);
-				
-				messages.add(new Message(jsonObj.getString("username"),
-										 jsonObj.getString("timestamp"),
-										 jsonObj.getString("message")));
+
+				messages.add(new Message(jsonObj.getString("username"), jsonObj
+						.getString("timestamp"), jsonObj.getString("message")));
 			}
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void onPostReturn(String URL, String response) {
 		if (URL.equals(Server.MESSAGES_URL)) {
 			addMessages(response);
 			displayUpdate();
-		}
-		else if (URL.equals(Server.SUBMIT_URL)) {
-			
+		} else if (URL.equals(Server.SUBMIT_URL)) {
+			processMessagePostResponse(response);
 		}
 	}
 
 	public void submitMessage(String msg_data) {
-    	String[] sa = new String[2];
-    	sa[0] = Server.SUBMIT_URL; 
-    	HashMap<String, String> submitVals = new HashMap<String, String>();
-    	submitVals.put("username", username);
-    	submitVals.put("password", password);
-    	submitVals.put("topic_id", Integer.toString(topicId));
-    	submitVals.put("message", msg_data);
-    	sa[1] = new JSONObject(submitVals).toString();
-    	
-    	new SubmitMessageRequestTask(this).execute(sa);
-    
+		String[] sa = new String[2];
+		sa[0] = Server.SUBMIT_URL;
+		HashMap<String, String> submitVals = new HashMap<String, String>();
+		submitVals.put("username", username);
+		submitVals.put("password", password);
+		submitVals.put("topic_id", Integer.toString(topicId));
+		submitVals.put("message", msg_data);
+		sa[1] = new JSONObject(submitVals).toString();
+
+		new SubmitMessageRequestTask(this).execute(sa);
+
 	}
-	
-	public void displayUpdate() { 
-		for (Message msg: messages.getMessages()) { 
+
+	public void displayUpdate() {
+		for (Message msg : messages.getMessages()) {
 			LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		    View v = inflater.inflate(R.layout.chat_activity, null);
+			View v = inflater.inflate(R.layout.chat_activity, null);
 
-		    // Find the ScrollView 
-		    ScrollView sv = (ScrollView) v.findViewById(R.id.chat_scroll_view);
+			// Find the ScrollView
+			ScrollView sv = (ScrollView) v.findViewById(R.id.chat_scroll_view);
 
-		    // Create a LinearLayout element
-		    LinearLayout ll = new LinearLayout(this);
-		    ll.setOrientation(LinearLayout.VERTICAL);
-		    ll.setPadding(15, 15, 15, 15);
+			// Create a LinearLayout element
+			LinearLayout ll = new LinearLayout(this);
+			ll.setOrientation(LinearLayout.VERTICAL);
+			ll.setPadding(15, 15, 15, 15);
 
-		    // Add text
-		    TextView name_msg1 = new TextView(this);
-		    name_msg1.setText(msg.getUsername() + ":  "+ msg.getMessage());
-		    ll.addView(name_msg1);
-		    
-		    TextView ts = new TextView(this);
-		    ts.setText(msg.getTimestamp().toString());
-		    ll.addView(ts);
-		    
-		   
-		    // Add the LinearLayout element to the ScrollView
-		    sv.addView(ll);
+			// Add text
+			TextView name_msg1 = new TextView(this);
+			name_msg1.setText(msg.getUsername() + ":  " + msg.getMessage());
+			ll.addView(name_msg1);
 
-		    // Display the view
-		    setContentView(v);
+
+			// Add the LinearLayout element to the ScrollView
+			sv.addView(ll);
+
+			// Display the view
+			setContentView(v);
 		}
 
+	}
+
+	public void processMessagePostResponse(String response) {
+		Log.d("Response: ", response);
+		try {
+			JSONObject jo = new JSONObject(response);
+			Integer errorCode = (Integer) jo.get("error");
+			if (Server.RESPONSE_CODES.containsKey(errorCode)) {
+				showMessage(Server.RESPONSE_CODES.get(errorCode));
+				return;
+			} else {
+				Log.d("Result", String.valueOf(errorCode));
+				showMessage("The server reported an unknown error.");
+				return;
+			}
+		} catch (JSONException e) {
+			// There was no error on the object. Hooray.
+		}
 	}
 }
